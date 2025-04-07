1ff7b82b4338744783902b15231818734d710922
Twisngm <chduaahd24@naver.com>
2025-04-07 21:46:51 +0900

    플레이어 소스 편집, 임시 플랫폼 생성

--- GameEngine/Assets/Script_Player/Player.cs
+++ GameEngine/Assets/Script_Player/Player.cs
@@ -1,20 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    [SerializeField] private InputActionAsset Input;
+    [SerializeField] private double Maxspeed; // 이동속도 제한을 위한 변수
+    private InputAction MoveAction; // 이동 (매니저에서 Y축 이동에 대한 재정의 필요)
     Rigidbody2D Rigidbody;
+
+    Vector2 MoveDirection;
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody2D>();
+        MoveAction = Input.FindActionMap("Player").FindAction("Move");
+        MoveAction.performed += Move_perform;
     }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        
+        Move(MoveDirection);
     }
-    void OnMove(Vector2 Direction)
+
+    void Move_perform(InputAction.CallbackContext obj) //콜백 호출과 실제 이동 분리
     {
-        Rigidbody.AddForce(Direction);
+        MoveDirection = obj.ReadValue<Vector2>();
+        Debug.Log(MoveDirection.x + " " + MoveDirection.y);
+    }
+    void Move(Vector2 Direction)
+    {
+        Rigidbody.linearVelocityX = Direction.x;
     }
 }
