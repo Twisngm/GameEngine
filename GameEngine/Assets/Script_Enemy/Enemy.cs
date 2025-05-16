28a6c76911a3aaba7d2dcf65a5ef6007af2b3e70
522525 <522525@naver.com>
2025-05-16 14:17:24 +0900

    몬스터 기본 클래스

---
+++ GameEngine/Assets/Script_Enemy/Enemy.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+//using Unity.Behavior;
+
+//[BlakcboardEnum]
+public enum EnemyState
+{
+    Idle,
+    Patrol,
+    Wander,
+    Chase,
+    Attack
+}
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyState state;
+
+    [SerializeField] protected float health;
+    [SerializeField] protected float damage;
+    [SerializeField] protected float speed;
+
+    protected Animator animator;
+    protected bool isDead = false;
+    
+    // 공격하는 기본 메서드 자식 클래스에서 구현
+    public virtual void Attack()
+    {
+        Debug.Log("DefaultAttack");
+        // 공격 로직
+    }
+
+    // 사망하는 기본 메서드
+    public virtual void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        animator.SetTrigger("Die");
+        Destroy(gameObject, 1f);  // 죽은 후 1초 뒤에 삭제
+    }
+
+    // 이동하는 기본 메서드
+    public virtual void Move(Vector3 target)
+    {
+        // 타겟으로 이동
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    // 데미지 계산하는 기본 메서드
+    public virtual void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+    }
+}
